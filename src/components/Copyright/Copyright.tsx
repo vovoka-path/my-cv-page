c6f8a97f822112240cfca4bb2a865c0f6fd0cbc5
vovoka <vovoka.path@gmail.com>
2022-11-19 23:42:19 +0300

    feat: styled Footer

--- src/components/Copyright/Copyright.tsx
+++ src/components/Copyright/Copyright.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import MuiLink from '@mui/material/Link';
 
+// import Logo from '../Logo';
+
 type CopyrightProps = {
   author: string;
   authorLink: string;
@@ -11,11 +13,11 @@ const Copyright = ({ author, authorLink }: CopyrightProps) => {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
       {'Copyright © '}
+      {new Date().getFullYear()}{' '}
       <MuiLink color="inherit" href={authorLink}>
         {author}
-      </MuiLink>{' '}
-      {new Date().getFullYear()}
-      {'.'}
+      </MuiLink>
+      {/* <Logo size="16px" /> */}
     </Typography>
   );
 };
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { useSiteMetadata } from '../../hooks/use-site-metadata';
 
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+
 import Copyright from '../Copyright';
-import Logo from '../Logo';
 
 const Footer: React.FC = () => {
   const { author, authorLink } = useSiteMetadata();
 
   return (
     <footer>
-      <Logo size="16px" />
-      <Copyright author={author} authorLink={authorLink} />
+      <Box px={{ xs: 3, sm: 10 }} py={{ xs: 5, sm: 10 }}>
+        <Container maxWidth="lg">
+          <Copyright author={author} authorLink={authorLink} />
+        </Container>
+      </Box>
     </footer>
   );
 };
