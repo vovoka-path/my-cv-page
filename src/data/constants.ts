c97fe45c257e552dc93fb233a21ab3e76df74be4
vovoka <vovoka.path@gmail.com>
2022-12-09 00:51:29 +0300

    refactor: contants.ts

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,19 +1,1 @@
 export const TO_TOP_ANCHOR = 'back-to-top-anchor';
-export const COLOR = {
-  // LOGO: '#fec374',
-  // LOGO: '#282f4b',
-  LOGO: '#2196F3',
-  // LOGO: '#FF9800',
-  BG: '#d9dee1',
-  HEADER_TEXT: '#303e5b',
-};
-// export const FOOTER = {
-//   HEIGHT: '180px',
-// };
-// export const HEADER = {
-//   HEIGHT: '68.5px',
-// };
-// export const VIEW_MODE = {
-//   MOBILE: 'mobile',
-//   DESKTOP: 'desktop',
-// };
