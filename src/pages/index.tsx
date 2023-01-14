cd38fcc46f54d7f6871369d57901a23a3c488718
vovoka <vovoka.path@gmail.com>
2023-01-15 01:07:51 +0300

    refactor: remove comments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Link, PageProps } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
@@ -15,22 +16,29 @@ import useSiteMetadata from '../hooks/useSiteMetadata';
 const title = 'About me';
 
 const textStyles = {
+  common: {
+    width: { xs: '100%', md: '50%' },
+  },
   title: {
-    padding: '0 0 20px',
+    padding: '0 0 24px',
     fontWeight: 700,
     color: Theme1.palette.secondary.dark,
     textShadow: '1px 3px 0 #ffffff, -1px -3px 0 #ffffff',
   },
   subTitle: {
-    padding: '0 0 20px',
+    padding: '0 0 24px',
     color: Theme1.palette.secondary.dark,
     textShadow: '1px 2px 0 #ffffff, -1px -2px 0 #ffffff',
   },
   text: {
-    padding: '0 0 40px',
+    padding: '0 0 24px',
     color: Theme1.palette.secondary.contrastText,
     textShadow: '1px 1px 0 #ffffff, -1px -1px 0 #ffffff',
   },
+};
+
+const linkStyles = {
+  textDecoration: 'none',
 };
 
 const btnStyles = {
@@ -38,6 +46,15 @@ const btnStyles = {
   margin: '10px 20px 10px 0',
   minWidth: '105px',
   fontWeight: 700,
+  color: Theme1.palette.secondary.contrastText,
+  textShadow: '1px 1px 0 #ffffff, -1px -1px 0 #ffffff',
+};
+
+const heroImgStyles = {
+  alignItem: 'right',
+  // paddingTop: 6,
+  borderRadius: '50%',
+  borderBottom: '18px solid #FF9800',
 };
 
 const IndexPage: React.FC<PageProps> = () => {
@@ -47,30 +64,44 @@ const IndexPage: React.FC<PageProps> = () => {
     <Layout>
       <Container maxWidth="lg">
         <Container maxWidth="lg">
-          <Box py={{ xs: 6, md: 12 }}>
-            <Typography variant="h2" sx={{ ...textStyles.title, width: { xs: '100%', md: '50%' } }}>
+          <Box
+            sx={{
+              mt: { xs: 6, md: 6 },
+              mb: { xs: 2, md: 1 },
+              width: { xs: 196, sm: 256 },
+              height: { xs: 196, sm: 256 },
+            }}
+          >
+            <StaticImage
+              src="../images/VP_transparent.png"
+              alt="Frontend developer photo"
+              placeholder="none"
+              layout="constrained"
+              imgStyle={heroImgStyles}
+              // width={{ xs: 196, sm: 256 }}
+              // height={{ xs: 196, sm: 256 }}
+            />
+          </Box>
+          <Box py={{ xs: 2, md: 2 }}>
+            <Typography variant="h2" sx={{ ...textStyles.common, ...textStyles.title }}>
               My name is {author.split(' ')[0]}
             </Typography>
-            <Typography variant="h3" sx={textStyles.subTitle}>
-              I&apos;m frontend developer
+            <Typography variant="h3" sx={{ ...textStyles.common, ...textStyles.subTitle }}>
+              Frontend developer
             </Typography>
-            <Typography
-              variant="body1"
-              sx={{ ...textStyles.text, width: { xs: '100%', md: '50%' } }}
-            >
-              I developed over 15 web applications in 2022 using React, Javascript, Typescript and
-              Node.js. I can to deliver results within project&apos;s resources and time
-              constraints.
+            <Typography variant="body1" sx={{ ...textStyles.common, ...textStyles.text }}>
+              Developed over 10 web applications in 2022 using React, Typescript/Javascript and
+              Node.js. Open to relocation or remote work.
             </Typography>
             <Box>
-              <Link style={{ textDecoration: `none` }} to={'/contacts'}>
-                <Button variant="outlined" sx={btnStyles} disableElevation>
-                  Contacts
+              <Link style={linkStyles} to={'/cv'}>
+                <Button variant="contained" sx={btnStyles} disableElevation>
+                  CV
                 </Button>
               </Link>
-              <Link style={{ textDecoration: `none` }} to={'/cv'}>
-                <Button variant="contained" sx={{ ...btnStyles, color: 'white' }} disableElevation>
-                  CV
+              <Link style={linkStyles} to={'/contacts'}>
+                <Button variant="outlined" sx={btnStyles} disableElevation>
+                  Contacts
                 </Button>
               </Link>
             </Box>
